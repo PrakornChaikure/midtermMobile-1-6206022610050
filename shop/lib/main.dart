e9e0ac8f5afc06d853c144b1d3d892f41b6f6196
PrakornChaikure <6206022610050@fitm.kmutnb.ac.th>
2022-03-02 15:58:48 +0700

    Test

---
+++ shop/lib/main.dart
@@ -0,0 +1,140 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/gestures.dart';
+import 'detail.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  TextEditingController name = TextEditingController();
+  TextEditingController price = TextEditingController();
+  TextEditingController dork = TextEditingController();
+  TextEditingController cnfpassword = TextEditingController();
+  TextEditingController mobile = TextEditingController();
+
+  @override
+  void initState() {
+    name.text = ""; //innitail value of text field
+    price.text = "";
+    dork.text = "";
+    cnfpassword.text = "";
+    mobile.text = "";
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("CIRRLE"),
+        ),
+        body: Container(
+            constraints: BoxConstraints.expand(),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage("assets/images/wall.jpg"),
+                    fit: BoxFit.cover)),
+            padding: EdgeInsets.all(10),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset(
+                    "assets/images/logo.png",
+                    height: 80,
+                  ),
+                  TextField(
+                      controller: name,
+                      decoration: InputDecoration(
+                        labelText: "ENTER NAME",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'Enter you NAME',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.person, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(height: 8),
+                  TextField(
+                      controller: price,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "ENTER PRICE",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: 'ENTER YOUR PRICE',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.email, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  Container(height: 8),
+                  TextField(
+                      controller: dork,
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: "ENTER DORK",
+                        labelStyle: const TextStyle(color: Colors.white),
+                        hintText: '6 month    10 month    24 month',
+                        hintStyle: const TextStyle(color: Colors.white),
+                        prefixIcon: Icon(Icons.lock, color: Colors.white),
+                        border: myinputborder(),
+                        enabledBorder: myinputborder(),
+                        focusedBorder: myfocusborder(),
+                      )),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => WelcomePage(
+                              n: name.text,
+                              w: price.text,
+                              e: dork.text,
+                            ),
+                          ),
+                        );
+                      },
+                      child: Text('Go Next Page'))
+                ],
+              ),
+            )));
+  }
+
+  OutlineInputBorder myinputborder() {
+    //return type is OutlineInputBorder
+    return OutlineInputBorder(
+        //Outline border type for TextFeild
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 3,
+        ));
+  }
+
+  OutlineInputBorder myfocusborder() {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+        borderSide: BorderSide(
+          color: Colors.white,
+          width: 3,
+        ));
+  }
+  //create a function like this so that you can use it wherever you want
+}
